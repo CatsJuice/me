0faeddd150281b73d305166a6c70c56fe33f5c06
CatsJuice <wp1298554944@hotmail.com>
2023-06-25 16:26:59 +0800

    remove unnecessary codes

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const appName = 'Vitesse for Nuxt 3'
-export const appDescription = 'Vitesse for Nuxt 3'
+export const appName = 'CatsJuice'
+export const appDescription = 'Profile of CatsJuice'
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,5 @@
 <template>
-  <main class="px-10 py-20 text-center">
+  <main>
     <slot />
-    <Footer />
-    <div class="mx-auto mt-5 text-center text-sm opacity-25">
-      [Default Layout]
-    </div>
   </main>
 </template>
